398272a183da4978d159a9f4da13e79e26fc7e0b
김미희 <opalgml@naver.com>
2020-07-06 22:00:37 +0900

    데이터 뿌리기

--- app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.listview_20200706.R
 import kr.co.tjoeun.listview_20200706.datas.Student
 
@@ -14,7 +15,8 @@ class StudentAdapter(val mContext: Context, val resId: Int, val mList: List<Stud
 //    화면을 그려줄 때 사용하는 객체
     val inf = LayoutInflater.from(mContext)
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View { //getView 는 한 줄의 데이터를 가져옴
+//        position파라미터는 몇 번의 데이터에 내용을 추가할지에 대한 파라미터값이 설정된다.
 
         var tempRow = convertView
 
@@ -27,8 +29,24 @@ class StudentAdapter(val mContext: Context, val resId: Int, val mList: List<Stud
 
 //        진짜 row에는 절대 null이 아닌 tempRow를 복사. (null인 경우 tempRow를 채우는 전처리작업을 했으므로 null일 수 없음.)
         val row = tempRow!!
-//        row 뿌려달라고 return 처리
 
+//        row 내용을 실제에 맞게 변경
+        val data = mList[position] //코틀린에서 ArrayList 값을 가져오는 방법 -> List.[ArrayList Number] (Ex. java -> List.get(index))
+
+//        실제 내용(row 내용) 을 가져옴
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val address = row.findViewById<TextView>(R.id.addressTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        데이터를 반영한다.
+        nameTxt.text = data.name
+        address.text = data.address
+
+        val age = 2020 - data.birthYear +1
+        ageTxt.text = "${age}세"
+
+
+//        row 뿌려달라고 return 처리 (결과로 지정)
         return row
-    }
+}
 }
