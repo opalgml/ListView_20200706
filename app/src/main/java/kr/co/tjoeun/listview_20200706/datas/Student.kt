396578510d98147493998f6a85a7eb0ef872883a
김미희 <opalgml@naver.com>
2020-07-06 20:52:42 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/datas/Student.kt
@@ -0,0 +1,7 @@
+package kr.co.tjoeun.listview_20200706.datas
+
+//신규 생성 시 class Stuent {} 로 클래스 파일이 생성된다.
+//생성자 선언 시 class Stuent(){} <- 괄호 추가해주면 생성자를 만든 것.
+//실습 클래스는 생성자에 String, Int 타입의 총 3개 매개변수를 추가해줌
+class Student(val name: String, val address: String, val birthYear:Int) {
+}
